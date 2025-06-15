c4aa7a3e9545142d869de04c4923e758733ba92b
Ian Tumulak <edden87@gmail.com>
2025-06-15 08:54:41 +0800

    make it italic

--- client/src/pages/Home/HeroSection.jsx
+++ client/src/pages/Home/HeroSection.jsx
@@ -13,7 +13,7 @@ export default () => {
                     <h1 className="mx-auto lg:mx-0 text-6xl lg:text-8xl font-black color-base w-fit">Hi, I'm Ian<span className="text-brand">.</span></h1>
                 </Reveal>
                 <Reveal width="100%">
-                    <h2 className="mx-auto lg:mx-0 w-max text-3xl lg:text-4xl">I build <span className="text-brand font-bold">awesome things</span>.</h2>
+                    <h2 className="mx-auto lg:mx-0 w-max text-3xl lg:text-4xl">I build <span className="text-brand font-bold italic">awesome things</span>.</h2>
                 </Reveal>
             </div>
         </Section>
